ad37176335034cd433417052a246efeadda7c973
Noah Trần <trananhquan1009@gmail.com>
2026-08-22 00:22:12 +0700

    chore: configure as zig library, remove standalone build derivations

--- flake.nix
+++ flake.nix
@@ -22,14 +22,14 @@
         system,
         ...
       }: let
-        isLinux = pkgs.stdenv.isLinux;
+        isLinux = pkgs.stdenv.hostPlatform.isLinux;
         pkgsMusl =
           if isLinux
           then pkgs.pkgsMusl
           else null;
 
         zig = inputs.zig-overlay.packages.${system}."0.16.0" or pkgs.zig;
-        zon2nix = inputs.zon2nix.packages.${system}.default;
+        zon2nix = inputs.zon2nix.packages.${system}.zon2nix;
 
         mkDevShell = p:
           p.mkShell {
@@ -53,24 +53,6 @@
           p.go
         ];
 
-        mkZigBuild = name: target: p:
-          p.stdenv.mkDerivation {
-            inherit name;
-            src = ./.;
-            nativeBuildInputs = mkBuildDeps p;
-            buildPhase = ''
-              export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
-              zig build -Doptimize=ReleaseFast ${
-                if target != ""
-                then "-Dtarget=${target}"
-                else ""
-              }
-            '';
-            installPhase = ''
-              mkdir -p $out
-              cp -r zig-out/* $out/ 2>/dev/null || true
-            '';
-          };
       in {
         formatter = pkgs.alejandra;
 
@@ -80,6 +62,7 @@
               name = "uWebZockets-test-default";
               src = ./.;
               nativeBuildInputs = mkBuildDeps pkgs;
+              dontConfigure = true;
               buildPhase = ''
                 export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
                 zig build test --summary all
@@ -92,6 +75,7 @@
               name = "uWebZockets-test-musl";
               src = ./.;
               nativeBuildInputs = mkBuildDeps pkgsMusl;
+              dontConfigure = true;
               buildPhase = ''
                 export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
                 zig build test --summary all
@@ -108,35 +92,6 @@
             musl = mkDevShell pkgsMusl;
           };
 
-        packages =
-          {
-            default = mkZigBuild "uWebZockets-default" "" pkgs;
-
-            "windows-x86_64" = mkZigBuild "uWebZockets-windows-x86_64" "x86_64-windows-gnu" pkgs;
-            "macos-x86_64" = mkZigBuild "uWebZockets-macos-x86_64" "x86_64-macos" pkgs;
-            "macos-aarch64" = mkZigBuild "uWebZockets-macos-aarch64" "aarch64-macos" pkgs;
-
-            "linux-x86_64-gnu" = mkZigBuild "uWebZockets-linux-x86_64-gnu" "x86_64-linux-gnu" pkgs;
-            "linux-aarch64-gnu" = mkZigBuild "uWebZockets-linux-aarch64-gnu" "aarch64-linux-gnu" pkgs;
-
-            "linux-x86_64-musl" = mkZigBuild "uWebZockets-linux-x86_64-musl" "x86_64-linux-musl" pkgs;
-            "linux-aarch64-musl" = mkZigBuild "uWebZockets-linux-aarch64-musl" "aarch64-linux-musl" pkgs;
-          }
-          // pkgs.lib.optionalAttrs isLinux {
-            musl = pkgsMusl.stdenv.mkDerivation {
-              name = "uWebZockets-musl-native";
-              src = ./.;
-              nativeBuildInputs = mkBuildDeps pkgsMusl;
-              buildPhase = ''
-                export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
-                zig build -Doptimize=ReleaseFast
-              '';
-              installPhase = ''
-                mkdir -p $out
-                cp -r zig-out/* $out/ 2>/dev/null || true
-              '';
-            };
-          };
       };
     };
 }
