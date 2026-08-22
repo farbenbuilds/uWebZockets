04baead917b94cc8004f3c4d2681239a2467167c
Noah Trần <trananhquan1009@gmail.com>
2026-08-22 07:36:19 +0700

    build: convert to library, strip comments, add flake packages, bump v… (#5)
    ...0.1.0
    
    * build: convert to library, strip comments, add flake packages, bump v0.1.0
    
    * build: add LICENSE and README.md to package paths

---
+++ build.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const mod = b.addModule("uWebZockets", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+    });
+
+    const lib = b.addStaticLibrary(.{
+        .name = "uWebZockets",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/root.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "uWebZockets", .module = mod },
+            },
+        }),
+    });
+
+    b.installArtifact(lib);
+
+    const mod_tests = b.addTest(.{
+        .root_module = mod,
+    });
+    const run_mod_tests = b.addRunArtifact(mod_tests);
+
+    const lib_tests = b.addTest(.{
+        .root_module = lib.root_module,
+    });
+    const run_lib_tests = b.addRunArtifact(lib_tests);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_mod_tests.step);
+    test_step.dependOn(&run_lib_tests.step);
+}
