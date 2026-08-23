8f6d7f07bc20d07ccba2a926ff169d2bbcfa7fcb
Noah Trần <trananhquan1009@gmail.com>
2026-08-23 09:23:43 +0700

    fix: use linguist-detectable dummy files to represent submodule languages

---
+++ .linguist-overrides/dummy.cpp
@@ -0,0 +1,3 @@
+// Dummy file to instruct GitHub Linguist to display C++ in the repository stats
+// This represents the vendored C++ code in the submodules (which Linguist ignores).
+int linguist_dummy_cpp() { return 0; }
---
+++ .linguist-overrides/dummy.go
@@ -0,0 +1,5 @@
+// Dummy file to instruct GitHub Linguist to display Go in the repository stats
+// This represents the vendored Go code in the submodules (which Linguist ignores).
+package dummy
+
+func LinguistDummyGo() int { return 0 }
