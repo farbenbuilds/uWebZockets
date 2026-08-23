9033299c632feecd1ea091d90d6e39f31600470a
Noah Trần <trananhquan1009@gmail.com>
2026-08-23 18:55:19 +0700

    fix: bypass arocc pragma bug in openssl headers

--- src/c.h
+++ src/c.h
@@ -3,6 +3,10 @@
 #include <netinet/in.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <openssl/base.h>
+#undef OPENSSL_GNUC_CLANG_PRAGMA
+#define OPENSSL_GNUC_CLANG_PRAGMA(arg)
+
 #include <openssl/ssl.h>
 #include <openssl/crypto.h>
 #include <lsquic.h>
