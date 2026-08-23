ce57d40fb0d2506170d91e5df911e9a5efa23fa1
Noah Trần <trananhquan1009@gmail.com>
2026-08-23 23:36:31 +0700

    chore: track deps.nix and remove unused headers

--- src/c.h
+++ src/c.h
@@ -1,8 +1,6 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <stddef.h>
-#include <stdint.h>
 #include <openssl/base.h>
 #undef OPENSSL_GNUC_CLANG_PRAGMA
 #define OPENSSL_GNUC_CLANG_PRAGMA(arg)
