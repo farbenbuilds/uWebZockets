56734c3775ddfcb643c6260da4b6ae673d45be9c
Noah Trần <trananhquan1009@gmail.com>
2026-08-24 23:08:52 +0700

    feat: update root api and export lifecycle methods
    Exposed App, Request, Response, end_response, and write_chunk to provide a clean API for end users.

--- src/root.zig
+++ src/root.zig
@@ -2,8 +2,15 @@
 // follows data-oriented design by separating data structures from behavior.
 
 pub const App = @import("router/app.zig").App;
+pub const init_app = @import("router/app.zig").init_app;
+pub const deinit_app = @import("router/app.zig").deinit_app;
+pub const add_route = @import("router/app.zig").add_route;
+pub const listen = @import("router/app.zig").listen;
+pub const run = @import("router/app.zig").run;
 pub const Request = @import("http/request.zig").Request;
 pub const Response = @import("http/response.zig").Response;
+pub const end_response = @import("http/response.zig").end;
+pub const write_chunk = @import("http/response.zig").write_chunk;
 
 comptime {
     _ = @import("test.zig");
