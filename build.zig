fa1d1e392504de6266785e662a3e1f27d38b443a
Noah Trần <trananhquan1009@gmail.com>
2026-08-26 02:53:35 +0700

    fix: add lsquic dependency on boringssl and remove manual evex512 flags

--- build.zig
+++ build.zig
@@ -63,7 +63,7 @@ pub fn build(b: *std.Build) void {
     // 3. libdeflate
     const deflate_src = "vendor/libdeflate";
     const deflate_build_dir = b.pathJoin(&.{ vendor_build, "libdeflate" });
-    const deflate_cmake = b.addSystemCommand(&.{ "cmake", "-B", deflate_build_dir, "-S", deflate_src, "-GNinja", "-DCMAKE_BUILD_TYPE=Release", "-DLIBDEFLATE_BUILD_GZIP=OFF", "-DLIBDEFLATE_BUILD_SHARED_LIB=OFF", "-DCMAKE_C_FLAGS=-mno-evex512", zig_cc, zig_cxx, zig_asm });
+    const deflate_cmake = b.addSystemCommand(&.{ "cmake", "-B", deflate_build_dir, "-S", deflate_src, "-GNinja", "-DCMAKE_BUILD_TYPE=Release", "-DLIBDEFLATE_BUILD_GZIP=OFF", "-DLIBDEFLATE_BUILD_SHARED_LIB=OFF", zig_cc, zig_cxx, zig_asm });
     const deflate_ninja = b.addSystemCommand(&.{ "ninja", "-C", deflate_build_dir });
     deflate_ninja.step.dependOn(&deflate_cmake.step);
 
